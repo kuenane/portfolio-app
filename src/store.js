5fd21cb9ec84380cefba09dc66545362630732b3
Kuenane <lebohang.kuenane44@gmail.com>
2021-04-02 15:54:52 +0200

    initial commit

---
+++ src/store.js
@@ -0,0 +1,164 @@
+import Vue from "vue";
+import Vuex from "vuex";
+/*import { stat } from "fs";*/
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    profile: {
+        name: "Lebohang",
+        surname: "Kuenane",
+        birthDay: new Date(1995, 6, 7),
+        startDescription: "Hey, I'm Lebo",
+        description:
+            ", a software engineer based in Johannesburg, South-Africa. Full-stack developer with industry experience building websites and web applications. I work with NodeJs and Django on the backend, React and Vue on the frontend. ",
+    },
+    technologies: {
+        programming:  [
+            { name: "Python & Django", level: 90 , },
+            { name: "HTML & (S)CSS", level: 92 },
+            { name: "Javascript (ES6)", level: 80 },
+            { name: "Typescript", level: 60 }
+        ],
+        frameworks: [
+            { name: "Vue.js & Nuxt.js", level: 65 },
+            { name: "AngularJS / Angular2+", level: 85 },
+            { name: "Tailwind.css", level: 80 },
+            { name: "Bootstrap", level: 80 },
+            { name: "Node.js & Express.js", level: 68 },
+        ],
+        tools:[
+            { name: "Digital Ocean" },
+            { name: "Amazon AWS" },
+            { name: "Maven" },
+            { name: "Webpack" },
+            { name: "Netlify" },
+            { name: "Wordpress" }
+        ]
+    },
+    projects: [{
+        name: "LeafPic Gallery",
+        description: "LeafPic is a fluid, material-designed alternative gallery, it also is ad-free and open source under GPLv3 license. I worked as the major designer in this project, I designed and implemented features such as photo affix.",
+        link: "https://github.com/HoraApps/LeafPic",
+        icon: "leaf",
+        startDate: "Jan 7, 2016",
+        endDate: "Present",
+        developer: "HoraApps",
+        tags: [
+            { name: "#opensource" },
+            { name: "#android" },
+            { name: "#app" },
+            { name: "#gallery" }
+        ]
+    }, {
+        name: "portfolio",
+        description: "Portfolio made with Vue.js and Tailwind css! This portfolio is open source and you can find it on Github!",
+        link: "https://github.com/gilnd/portfolio",
+        icon: "address-card",
+        startDate: "Sep 20, 2019",
+        endDate: "Present",
+        developer: "gilbertndr",
+        tags: [
+            { name: "#opensource" },
+            { name: "#vue" },
+            { name: "#tailwindcss" },
+            { name: "#portfolio" }
+        ]
+    },{
+        name: "adbFI",
+        description: "abdFi is an Android app that lets you debug Android apps over Wi-Fi, without the cable connected to your PC!",
+        link: "https://github.com/gilnd/adbFI",
+        icon: "wifi",
+        startDate: "Sep 20, 2019",
+        endDate: "Present",
+        developer: "gilbertndr",
+        tags: [
+            { name: "#opensource" },
+            { name: "#android" },
+            { name: "#adb" }
+        ]
+    }
+    ], events: [
+        { 
+            name: "Vyudu Inc",
+            description: "I work in this company as a fullstack developer, I mostly use JavaEE with Spring Boot and Angular for frontend!",
+            role: "Full Stack Developer ",
+            type: "Work",
+            startDate: "Nov 2019",
+            endDate: "Present",
+            roles:[
+                {description:" Improved and maintained ANGULAR 2 CUSTOM FRAMEWORK following UI and UX guidelines given by Intesa SanPaolo." },
+                {description:" Developed and maintained a back office payment webapp with Angular2 for Intesa SanPaolo." },
+                {description:" Developed features and maintained web applications used by italian public administration." },
+                {description:" Migrated from mongoDB v3 to mongoDB v4 , updated to the latest libraries and maintained an insurance engine for Intesa SanPaolo Assicura." }
+            ]
+        },
+        { 
+            name: "Code For Fun",
+            description: "Xtphere Srl is a company which carries out innovative solutions for Web & Digital Technology, Mobile Experience, Document Process Management, Big Data and Cloud Migration.",
+            role: "Full Stack Developer ",
+            type: "Work",
+            startDate: "Jun 2018",
+            endDate: "Nov 2019",
+            roles:[
+                {description:" Developed features and maintained an insurance engine, made with JavaEE & AngularJS for Intesa SanPaolo Assicura." },
+                {description:" Migrated projects from old Libraries & tools to new ones and improved general performance." },
+                {description:" Interfaced with Miroglio Group on a monthly basis providing help and support for new features and improvements." },
+                {description:" Developed, tested and maintained cross platform web applications that work on various devices and browsers es: bigEYE."}
+            ]
+        }, { 
+            name: "Finchatbot",
+            description: "Edisoft Srl is a small web agency specialized in development of websites & web applications, it also offers high quality graphic design, online advertising and SEO optimization. ",
+            role: "Front End Developer ",
+            type: "Work",
+            startDate: "Gen 2017",
+            endDate: "Jun 2017",
+            roles:[
+                {description:" Maintained E-Commerce website and added new features, with Wordpress." },
+                {description:" Developed and maintained code for client websites, mostly using HTML, CSS, Javascript, Jquery & PHP." },
+            ]
+        }, { 
+            name: "Technify Africa",
+            description: " ",
+            role: "Student",
+            type: "education",
+            startDate: "Sep 2010",
+            endDate: "Jun 2016",
+            roles:[
+                { description:" In depth learning of OOP programing with C#. " },
+                { description:" In depth learning of HTML, CSS, JS & Jquery. " },
+                { description:" Learned basics of development of C & C++." },
+                { description:" Learned basics of networks and their structure. " }
+            ]
+        }
+    ], socials: [
+        {
+            name: "LinkedIn",
+            link: "https://linkedin.com/in/gilbertndr/",
+            icon: "linkedin" 
+        }, {
+            name: "Github",
+            link: "https://github.com/gilnd",
+            icon: "github" 
+        }, {
+            name: "Twitter",
+            link: "https://twitter.com/gilbertndr",
+            icon: "twitter" 
+        },   
+    ]
+},
+
+    getters: { age: state => {
+            let ageDiff = Date.now() - state.profile.birthDay;
+            let ageDate = new Date(ageDiff);
+            return Math.abs(ageDate.getUTCFullYear() - 1970);
+        },
+        profile: state => state.profile,
+        projects: state => state.projects,
+        technologies: state => state.technologies,
+        events: state => state.events,
+        socials: state => state.socials
+    },
+    mutations: {},
+    actions: {}
+});
---
+++ src/views/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="my-8 flex flex-col md:justify-center">
+    <div>
+      <h3 class="text-secondary-main text-lg font-bold">
+        ➔ Hey There, I&rsquo;m
+      </h3>
+      <h1 class="text-primary-main font-bold sm:text-5xl text-4xl">
+        {{ profile.name }} {{ profile.surname }}.
+      </h1>
+      <div
+        class="bg-primary-main text-background shadow-lg text-center rounded-sm"
+      >
+        <h1 class="font-bold md:text-3xl sm:text-2xl text-xl">
+          I create beautiful experiences
+        </h1>
+      </div>
+    </div>
+    <scene></scene>
+    <div class="w-full my-2">
+      <div class="text-label-info text-xl">
+        I&rsquo;m a software engineer based in Johannesburg, Gauteng,
+        South-Africa.<br />
+      </div>
+      <button
+        class="text-primary-main rounded-sm text-lg p-4 border-2 hover:bg-transparent-primary my-4"
+        onclick="location.href='mailto:gilbert.ndresaj@gmail.com'"
+      >
+        Get In Touch
+      </button>
+      <a href="https://resume.io/r/Bx5M43Kbe" target="_blank" download>
+        <button
+          class="ml-5 text-primary-main rounded-sm text-lg p-4 border-2 hover:bg-transparent-primary my-4"
+        >
+          Get Resume
+        </button>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import scene from "@/components/scene.vue";
+import { mapGetters } from "vuex";
+export default {
+  components: { scene },
+  computed: { ...mapGetters(["profile", "projects", "age"]) },
+};
+</script>
+<style></style>
